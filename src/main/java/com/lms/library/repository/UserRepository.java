558f9bc0d739cb19ca8e4c7260167db75bae7c5b
Samet Öztürk <0zturksamet4901@gmail.com>
2022-04-05 17:44:38 +0200

    Book Repository created

--- src/main/java/com/lms/library/repository/UserRepository.java
+++ src/main/java/com/lms/library/repository/UserRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.lms.library.domain.User;
+import com.lms.library.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 		
