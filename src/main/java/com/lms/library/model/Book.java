edfff2e946e72e569556b4bdcd874e21bbcfea2d
Samet Öztürk <0zturksamet4901@gmail.com>
2022-04-05 17:31:11 +0200

    book model created

---
+++ src/main/java/com/lms/library/model/Book.java
@@ -0,0 +1,70 @@
+package com.lms.library.model;
+
+import java.io.Serializable;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="book")
+@AllArgsConstructor
+@Getter
+@Setter
+public class Book implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+	
+	
+	
+	@NotNull(message = "*Please enter book title")
+	@NotBlank(message = "*Plese enter book title")
+	@Column(name="title")
+	
+	private String title;
+	
+	
+	@NotNull(message = "*Please enter book tag")
+	@NotBlank(message = " *Please enter book tag")
+	@Column(name="tag")
+	private String tag;
+	
+	@NotNull(message = " Please enter book authhors")
+	@NotBlank(message = " Please enter book authors")
+	@Column(name = "authors")
+	private String authors;
+	
+	@Column(name = "publisher")
+	private String publisher;
+	
+	@Column(name = " isbn")
+	private String isbn;
+	
+	@Column(name = " status")
+	private Integer status;
+	
+	@Column(name = "create_date")
+	private Date createDate;
+	
+	
+	@Column(name = "category")
+	private String category;
+	
+	
+	
+	
+}
---
+++ src/main/java/com/lms/library/model/User.java
@@ -0,0 +1,79 @@
+package com.lms.library.model;
+
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="users")
+public class User  {
+	
+	public User(@NotNull String displayName, @NotNull String username, @NotNull String password,
+			@NotNull Integer active, @NotNull String role, @NotNull Date createdDate, Date lastModifiedDate) {
+		super();
+		this.displayName = displayName;
+		this.username = username;
+		this.password = password;
+		this.active = active;
+		this.role = role;
+		this.createdDate = createdDate;
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+	
+	@NotNull
+	@Column(name = "display_name")
+	private String displayName;
+	
+	@NotNull
+	@Column(name = "username")
+	private String username;
+	
+	@NotNull
+	@Column(name = "password")
+	private String password;
+	
+	@NotNull
+	@Column(name = "active")
+	private Integer active;
+	
+	@NotNull
+	@Column(name = "role")
+	private String role;
+	
+	@NotNull
+	@Column(name = "created_date")
+	private Date createdDate;
+	
+	@Column(name = "last_modified_date")
+	private Date lastModifiedDate;
+
+	
+
+	
+	
+	
+	
+	
+	
+}
